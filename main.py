d882bc437ccdfd5dacb9bc72a4a185d0fec56a83
altruant <noellehamberis@gmail.com>
2020-10-07 11:13:49 -0400

    clear console

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import time
-
+from os import system, name
 def slowprint(str):
   for letter in str + '\n':
     sys.stdout.write(letter)
     sys.stdout.flush()
     time.sleep(1./10)
+def clear():
+    _ = system('clear')
 
+clear()
 slowprint("This is a test of slowprint")
