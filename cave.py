1ba436402436de6e3ef3c03a45c129cb908e5fbe
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-07 15:33:58 -0400

    added upper so user input can be either lower or uppercase

--- cave.py
+++ cave.py
@@ -16,7 +16,7 @@ player = players.Player()
 
 def use_item():
     slowprint('Do you want to use an item?')
-    item_select = input('Y/N \n')
+    item_select = input('Y/N \n').upper()
     if item_select == 'Y':
         print('Which item would you like to use?', player.items)
         selected_item = int(input(''))
@@ -41,19 +41,19 @@ def death(player):
 
 def cave():
     slowprint('You come across a cave deep in the forest, do you go into the cave?')
-    cave_cont = input('Y/N \n')
+    cave_cont = input('Y/N \n').upper()
     if cave_cont == 'Y':
         slowprint('As you descend the cave you hear a loud crash behind you, you look back to see that you are now trapped inside the cave. Do you panic?')
     elif cave_cont == 'N':
         slowprint('You continue deeper into the forrest')
-    panic = input('Y/N \n')
+    panic = input('Y/N \n').upper()
     if panic == 'Y':
         player.alive = False
         slowprint('While you panick you fail to notice that you caused a rockslide. You inevitable end up dying.')
         death(player)
     elif panic == 'N':
         slowprint('You continue down the cave. Surpisingly the inside of the cave is well lit up. You come accross a stray dog. Do you stop to pet it?')
-        pet_the_dog = input('Y/N \n')
+        pet_the_dog = input('Y/N \n').upper()
         if pet_the_dog == 'Y':
             player.hp -= 5
             slowprint(f"You approach the dog, the dog seems friendly at first but as you reach out to pet the dog, it lashes out and bites you. You take some damage, you are now at {player.hp} health. The dog runs away and you are left with a bite mark on your hand but you don\'t think it\'s a big deal.")
