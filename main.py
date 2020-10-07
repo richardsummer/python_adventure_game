c6e8be6493c54ec6d50f9992f1ffcf7cac3e34c6
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-10-07 11:24:43 -0400

    tweaked the slow print function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import time
 from os import system, name
+
 def slowprint(str):
   for letter in str + '\n':
     sys.stdout.write(letter)
     sys.stdout.flush()
-    time.sleep(1./10)
+    time.sleep(1./60)
 def clear():
     _ = system('clear')
 
