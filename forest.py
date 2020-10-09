99ff05a007242fc71f42c5df1a6257611ee48fec
A. James Lindsey <macbookair@MacBooks-MacBook-Air.local>
2020-10-09 11:49:25 -0400

    modified

--- forest.py
+++ forest.py
@@ -3,6 +3,7 @@ import time
 from os import system, name
 import players
 import cave
+import deep_forest
 
 def slowprint(str):
   for letter in str + '\n':
@@ -24,20 +25,20 @@ def forest():
     slowprint ("He's signaling for you to come follow him. Do you want to follow him?")
     forest_cont = input ('Y/N\n')
     if forest_cont == 'Y':
-        slowprint ("You follow the rabbit and he leads you to a cave. Do you want to enter?")
-        forest_cave = input ('Y/N\n')
-        if forest_cave == 'Y':
-            slowprint ("Good luck!")
-            # Brian Code
-        elif forest_cave == 'N':
-            slowprint ("You kill the rabbit because you don't trust him. Then you enter the cave.")
-
+        # slowprint ("You follow the rabbit and he leads you to a cave. Do you want to enter?")
+        # forest_cave = input ('Y/N\n')
+        # if forest_cave == 'Y':
+        #     #slowprint ("Good luck!")
+        #     cave.cave()
+        # elif forest_cave == 'N':
+        #     slowprint ("You kill the rabbit because you don't trust him. Then you enter the cave.")
+            cave.cave()
     elif forest_cont == 'N':
         slowprint ("Go in the opposite direction.")
         slowprint ("You happen to stumble upon an old abandoned house. Should you enter?")
         forest_house = input ('Y/N\n')
         if forest_house == 'Y':
-            slowprint ("You explore the house and come across a crest with gold coins.")
+            slowprint ("You explore the house and come across a chest with gold coins.")
             slowprint ("You ponder if you should take the coins or leave them there. Should you take them?")
             forest_coins = input ('Y/N\n')
             if forest_coins == 'Y':
@@ -46,14 +47,15 @@ def forest():
                 house_door = input ('A/B\n')
                 if house_door == 'A':
                     slowprint ("This leads you outside and you go deeper into the forest")
-                    #Richard code
+                    deep_forest.deepForest()
                 if house_door == 'B':
                     slowprint ("There is a bear in this room and he kills you.")
             elif forest_coins == 'N':
                 slowprint ("You leave them there and exit the house to go deeper into the forest.")
-                #Richard code
+                deep_forest.deepForest()
         elif forest_house == 'N':
             slowprint ("Continue to go deeper into the forest.")
-            #Richard code
+            deep_forest.deepForest()
+        
 
 forest()
